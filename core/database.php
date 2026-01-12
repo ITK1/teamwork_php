7243ee15165a57dc98629fed29e8a500a1d07f92
Linh <trantieulinh01012000@gmail.com>
2026-01-12 15:32:48 +0700

    up

--- core/database.php
+++ core/database.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$dbname = "shop_online";
+$dbname = "ql_khohang";
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 if (!$conn) die("Lỗi kết nối: " . mysqli_connect_error());
